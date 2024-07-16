d9cb4e1124170312e2fa5cd90f2525fcf6f85af8
winniekim1001 <winnie@stayge.io>
2024-07-16 11:47:48 +0900

    feat : 다국어드롭다운 기능 구현:

--- src/components/button/GlobalButton.tsx
+++ src/components/button/GlobalButton.tsx
@@ -5,6 +5,7 @@ import { ArrowIcon, GlobalIcon } from 'components/icons';
 import { useLocale } from 'store/locale/hooks/useLocale';
 import i18next from 'i18next';
 import { Locale } from 'store/locale/types';
+import { keyframes } from '@emotion/react';
 
 interface GlobalButtonProps {}
 
@@ -20,26 +21,46 @@ const GlobalButton: React.FC<GlobalButtonProps> = () => {
     <div style={{ position: 'relative' }}>
       <GlobalButtonWrap onClick={() => setIsOpen(!isOpen)}>
         <GlobalIcon color={Colors.Neutral900} />
-        <ArrowIcon color={Colors.Neutral900} />
+        <ArrowIcon
+          color={Colors.Neutral900}
+          style={{ rotate: isOpen ? '180deg' : '0deg' }}
+        />
       </GlobalButtonWrap>
-      {isOpen && (
-        <DropBox>
-          {LocaleData.map((data) => (
-            <LocaleBox
-              onClick={() => handleChangeLan(data.code as Locale)}
-              key={data.code}
-              isSeleted={data.code === String(locale)}
-            >
-              {data.locale}
-            </LocaleBox>
-          ))}
-        </DropBox>
-      )}
+
+      <DropBox isOpen={isOpen}>
+        {LocaleData.map((data) => (
+          <LocaleBox
+            onClick={() => handleChangeLan(data.code as Locale)}
+            key={data.code}
+            isSeleted={data.code === String(locale)}
+          >
+            {data.locale}
+          </LocaleBox>
+        ))}
+      </DropBox>
     </div>
   );
 };
 
 export default GlobalButton;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+`;
 
 const LocaleData = [
   { locale: '한국어', code: 'ko' },
@@ -53,10 +74,11 @@ const GlobalButtonWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 2px;
   cursor: pointer;
 `;
 
-const DropBox = styled.div`
+const DropBox = styled.div<{ isOpen: boolean }>`
   position: absolute;
   top: -20px;
   right: 0;
@@ -70,6 +92,9 @@ const DropBox = styled.div`
   background-color: ${Colors.White};
   box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.25);
   z-index: 10;
+  animation: ${({ isOpen }) => (isOpen ? fadeIn : fadeOut)} 0.1s ease-in;
+  visibility: ${({ isOpen }) => (isOpen ? 'visible' : 'hidden')};
+  transition: visibility 0.1s ease-out;
 
   @media (max-width: 768px) {
     margin-top: 52px;
